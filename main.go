4ddaac6bd19c94a3e563354850385052e5a9f271
ciper <anmolkarki101@gmail.com>
2023-11-21 14:03:21 +0545

    docs: Add comments and documentation to main.go
    - Document functions, variables, and sections in main.go for improved code readability.
    - Provide explanations for the purpose of different parts of the code.
    - Enhance comments for better understanding of the Wit.ai and Wolfram Alpha integration.

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 
 var wolframClient *wolfram.Client
 
+// printCommandEvents is a goroutine that prints command events for debugging purposes.
 func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticsChannel {
 		fmt.Println("Command Events	")
@@ -28,35 +29,52 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 }
 
 func main() {
+	// Load environment variables from the .env file
 	godotenv.Load(".env")
+
+	// Create a new Slacker bot instance with Slack API tokens
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
+	// Create Wit.ai client and Wolfram Alpha client
 	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
 	wolframClient = &wolfram.Client{AppID: os.Getenv("WOLFRAM_APP_ID")}
 
+	// Start a goroutine to print command events for debugging
 	go printCommandEvents(bot.CommandEvents())
 
+	// Define a command for querying Wolfram Alpha
 	bot.Command("query - <message>", &slacker.CommandDefinition{
-		Description: "Send any question to wolfram",
+		Description: "Send any question to Wolfram Alpha",
 		Examples:    []string{"who was the president of Nepal in 2017?"},
 		Handler: func(bc slacker.BotContext, r slacker.Request, w slacker.ResponseWriter) {
+			// Extract the message parameter from the command
 			query := r.Param("message")
 			fmt.Println(query)
+
+			// Use Wit.ai to parse the message and extract relevant information
 			msg, _ := client.Parse(&witai.MessageRequest{
 				Query: query,
 			})
+
+			// Extract the value from Wit.ai response
 			data, _ := json.MarshalIndent(msg, "", "    ")
 			rough := string(data[:])
 			value := gjson.Get(rough, "entities.wit$wolfram_search_query:wolfram_search_query.0.value")
 			answer := value.String()
+
+			// Query Wolfram Alpha for a spoken answer
 			res, err := wolframClient.GetSpokentAnswerQuery(answer, wolfram.Metric, 1000)
 			if err != nil {
 				log.Fatal(err)
 			}
 			fmt.Println(value)
+
+			// Reply to the user in the Slack channel
 			w.Reply(res)
 		},
 	})
+
+	// Set up a context and listen for incoming events
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	err := bot.Listen(ctx)
