1086632128f60e791c4f3848c9c21df2cc42a833
ciper <anmolkarki101@gmail.com>
2023-11-21 12:47:24 +0545

    Add function for printing Command Events

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/shomali11/slacker"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events	")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
 
 func main() {
 	godotenv.Load(".env")
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	go printCommandEvents(bot.CommandEvents())
 }
